bc62ed509c313b3b9aeedb864d39c4e6ed814768
aka <khaliqarrasyidabdul@gmail.com>
2025-08-24 23:02:33 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    // Ganti dengan URL domain-mu setelah deploy jika diperlukan,
+    // tapi '/api/upload.php' akan berfungsi dengan baik di Vercel.
+    apiEndpoint: '/api/upload.php'
+};
+
